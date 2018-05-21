d91995490ee7b94270ea66446b59daed08103bdc
mohamed riaad <mohammed.riaad@gmail.com>
2018-05-21 13:58:05 +0200

    first

---
+++ src/data/credentials.js
@@ -0,0 +1,20 @@
+/**
+ * @description FEND Project 8 : Neighborhood
+ * @description main component
+ * @author Mohamed Riaad
+ * @version 1.0
+ */
+
+/**
+ * @description my credentials for security reasons were kept away and for multi use  . 
+ */
+
+// forsquare client id and secret 
+
+export const CLIENT_ID = 'PODXK0FF2KJDN3B2R1JSDYI2PRFR1WZLI1VVJUCRIP0OU45Q';
+
+export const CLIENT_SECRET = 'WHN5N2D445VVITV1QTVMOBJYXIJWLOEWOCST10SJKNYBEXHB';
+
+// google maps key  
+
+export const MAP_KEY = 'AIzaSyAJjkGrYGR_mcuy7Z14ZJ-5rug5qUGW6Bw';
